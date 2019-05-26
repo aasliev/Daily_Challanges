04bc9ed6d6f1b2ebe5a7f24ba399573807da21d0
aasliev <asliev.asliddin@gmail.com>
2019-05-25 20:40:47 -0400

    Update stock_buy_and_sell.py

--- day3/stock_buy_and_sell.py
+++ day3/stock_buy_and_sell.py
@@ -1,4 +1,4 @@
-'''def find_max(a,n):
+def find_max(a,n):
     tmpMin,tmpMax,result = 0,0,''
     for i in range(1,n):
         if (a[i]>a[tmpMax]):
@@ -32,55 +32,3 @@ def main():
     a.clear()
         
 main()
-
-
-
-
-'''
-
-
-#code
-noProfCond = True
-
-def checkNoProfit():
-    global noProfCond
-    if noProfCond:
-        print ("No Profit", end="")
-    print()
-
-def printStr(buy, sell):
-    global noProfCond
-    string = "("+ str(buy)+" "+ str(sell)+") "
-    print(string, end="")
-    noProfCond = False
-
-def getBestStock(lst, listLen):
-    arr = list(map(int, lst))
-    buyStock = 0
-    lastDayCheck = False
-    for i in range(listLen-1):
-        if (arr[buyStock] > arr[i]):
-            buyStock = i
-        if (arr[i]>arr[i+1] or i == listLen -2) and arr[buyStock] < arr[i]:
-            if arr[i]>arr[i+1]:
-                printStr(buyStock, i)
-            else:
-                printStr(buyStock, i+1)
-                lastDayCheck = True
-            buyStock = i+1
-            
-    if arr[i] < arr[i+1] and (not lastDayCheck):
-        printStr(i, i+1)
-    arr.clear()
-
-def main():
-    numOfTestCases = int(input())
-    for i in range(numOfTestCases):
-        listSize = int(input())
-        lst = input().split()
-        getBestStock(lst, listSize)
-        checkNoProfit()
-        lst.clear
-        
-if __name__== "__main__":
-  main()
